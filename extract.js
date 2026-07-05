171868503fc1f07937207b1eda88c555e8318752
Mihika Sarwate <mihika@example.com>
2026-07-06 00:35:29 +0530

    Clean up repository, remove old dev scripts

--- extract.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('index.html', 'utf8');
-const scriptMatch = html.match(/<script>([\s\S]*?)<\/script>/);
-if (scriptMatch) {
-  fs.writeFileSync('temp_script.js', scriptMatch[1], 'utf8');
-  console.log("Script extracted");
-} else {
-  console.log("No script tag found");
-}
--- fix_console_error.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('index.html', 'utf8');
-
-// Omit designation from dbEmps so we don't trigger 400 Bad Request and scary console errors
-html = html.replace(/designation: \(e\.designation \|\| ''\)\.trim\(\) \|\| getDefaultEmployeeDesignation\(e\.role\),/, `// designation omitted because table doesn't have it`);
-
-fs.writeFileSync('index.html', html, 'utf8');
-console.log("Done");
